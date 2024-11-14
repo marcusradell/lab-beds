f18123d8dea0b74cd3c508a6166cbfe523aa78c8
Marcus Rådell <marcus@radell.net>
2024-11-14 09:20:11 +0100

    add onClick function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 export default function Home() {
+  function onClick() {
+    alert("Clicked!");
+  }
+
   return (
     <>
       <h1>Add a bed</h1>
       <input type="text" />
-      <button>Add bed</button>
+      <button onClick={onClick}>Add bed</button>
     </>
   );
 }
