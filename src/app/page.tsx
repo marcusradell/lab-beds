09530a72a6a2d02dadd2a9af4e23f427d7dd0f4f
Marcus Rådell <marcus@radell.net>
2024-11-14 09:17:57 +0100

    add action for adding a bed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1>Add a bed</h1>;
+  return (
+    <>
+      <h1>Add a bed</h1>
+      <input type="text" />
+      <button>Add bed</button>
+    </>
+  );
 }
